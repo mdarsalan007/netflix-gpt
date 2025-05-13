5a9829835eee9bdd464a51e9862b382a8addb0d2
mdarsalan007 <arsalansiddiquie007@gmail.com>
2025-05-13 10:26:50 +0530

    deploy using firebase

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import logo from "../assets/Logo-1.png"
 
 const Header = () => {
   return (
     <div  className="absolute  z-10 h-24 flex items-center ml-6" >
-      <img className=' w-40' src="src/assets/Logo-1.png"/>
+      <img className=' w-40' src={logo}/>
     </div>
   )
 }
